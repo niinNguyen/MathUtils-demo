f09868cbcb124dc182b110a84ff32778c2dc6658
niinNguyen <nguyennkse140132@fpt.edu.vn>
2020-07-10 10:13:22 +0700

    modify utils

--- src/org/niin/utils/MathUtils.java
+++ src/org/niin/utils/MathUtils.java
@@ -1,12 +1,14 @@
 package org.niin.utils;
 
 public class MathUtils {
-
-    public static long computeFactorial(int num) {
-        long result = 1;
-        for (int i = 1; i <= num; i++) {
-            result *= i;
+        
+        public static long computeFactorial(int num) {
+                long result = 1;
+                for (int i = 1; i <= num; i++) {
+                        result *= i;
+                }
+                return result;
         }
-        return result;
-    }
+
+        
 }
