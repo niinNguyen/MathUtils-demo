6de4fc11c096f53dd122c2405cf892059c9b43ca
niinNguyen <nguyennkse140132@fpt.edu.vn>
2020-07-12 01:49:06 +0700

    hello

--- src/org/niin/utils/MathUtils.java
+++ src/org/niin/utils/MathUtils.java
@@ -9,6 +9,9 @@ public class MathUtils {
                 }
                 return result;
         }
-
+        
+        public static int add(int num1, int num2) {
+                return num1 + num2;
+        }
         
 }
